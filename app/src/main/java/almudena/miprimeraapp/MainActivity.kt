b0a93a13a62c02f0de82444a08fd28a4deeaf958
AlmudenaRA <almudena.ruedas@gmail.com>
2020-10-12 22:19:25 +0200

    "Cambiado nombre iconos y añadido pantalla de presentación"

--- app/src/main/java/almudena/miprimeraapp/MainActivity.kt
+++ app/src/main/java/almudena/miprimeraapp/MainActivity.kt
@@ -2,10 +2,19 @@ package almudena.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //Creación del menú
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
